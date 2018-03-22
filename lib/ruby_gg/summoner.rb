4a2994c3b5addbadeb20a5fbb2082a96426e43bf
Manish Shahi <manish.shahi@mnsu.edu>
2018-03-22 22:42:31 +0000

    Method change and Bunch of private method added for better return type, return dataType with user, solo, flex and tt info

--- lib/ruby_gg/summoner.rb
+++ lib/ruby_gg/summoner.rb
@@ -9,30 +9,63 @@ module RubyGg
         def initialize(api_key, region)
             @api_key = api_key
             @region = region
-            @base_url = "https://#{@region}1.api.riotgames.com"
+            @base_url = "https://#{@region}.api.riotgames.com"
             @rank_url = '/lol/league/v3/positions/by-summoner/'
             @profile_url = '/lol/summoner/v3/summoners/'
         end
         
         def get_profile(id)
-            id.to_i
-            rank_url = "#{@base_url}#{@rank_url}#{id}?api_key=#{@api_key}"
-            profile_url = "#{@base_url}#{@profile_url}#{id}?api_key=#{@api_key}"
-            rank_payload = HTTParty.get(rank_url).parsed_response
-            rank_payload.each {|x| @idx = rank_payload.index(x) if x['queueType'].eql?'RANKED_SOLO_5x5'}
-            profile_payload = HTTParty.get(profile_url).parsed_response
             summoner_info = {}
-            summoner_info[:ign] = rank_payload[@idx]['playerOrTeamName']
-            summoner_info[:tier]= rank_payload[@idx]['tier']
-            summoner_info[:rank]= rank_payload[@idx]['rank']
-            summoner_info[:lp] = rank_payload[@idx]['leaguePoints']
-            summoner_info[:wins] = rank_payload[@idx]['wins']
-            summoner_info[:loss] = rank_payload[@idx]['losses']
-            summoner_info[:icon] = profile_payload['profileIconId']
-            summoner_info[:level] = profile_payload['summonerLevel']
+            summoner_info[:user] = get_user(id)
+            summoner_info[:solo] = get_solo(id)
+            summoner_info[:flex] = get_flex(id)
+            summoner_info[:tt] = get_tt(id)
             return summoner_info
         end
         
+        private
+        def get_user(id)
+            user_payload = HTTParty.get("#{@base_url}#{@profile_url}#{id.to_i}?api_key=#{@api_key}").parsed_response
+            user = {}
+            user_payload.each{|k,v| user[k.to_sym] = v}
+            return user
+        end
+            
+        private
+        def get_solo(id)
+            rank_payload = HTTParty.get("#{@base_url}#{@rank_url}#{id.to_i}?api_key=#{@api_key}").parsed_response
+            solo = {}
+            @solo = nil
+            rank_payload.each{|x| @solo = rank_payload.index(x) if x['queueType'].eql?'RANKED_SOLO_5x5'}
+            if not @solo.nil?
+                (rank_payload[@solo]).each {|k,v| solo[k.to_sym] = v if not "queueType".eql?k or "playerOrTeamName".eql?k or "playerOrTeamId".eql?k}
+            end
+            return solo
+        end
+        
+        private
+        def get_flex(id)
+            rank_payload = HTTParty.get("#{@base_url}#{@rank_url}#{id.to_i}?api_key=#{@api_key}").parsed_response
+            flex = {}
+            @flex = nil
+            rank_payload.each{|x| @solo = rank_payload.index(x) if x['queueType'].eql?'RANKED_FLEX_SR'}
+            if not @flex.nil?
+                (rank_payload[@flex]).each {|k,v| flex[k.to_sym] = v if not "queueType".eql?k or "playerOrTeamName".eql?k or "playerOrTeamId".eql?k}
+            end
+            return flex
+        end
+        
+        private
+        def get_tt(id)
+            rank_payload = HTTParty.get("#{@base_url}#{@rank_url}#{id.to_i}?api_key=#{@api_key}").parsed_response
+            tt = {}
+            @tt = nil
+            rank_payload.each{|x| @tt = rank_payload.index(x) if x['queueType'].eql?'RANKED_TT_SR'}
+            if not @tt.nil?
+                (rank_payload[@tt]).each {|k,v| tt[k.to_sym] = v if not "queueType".eql?k or "playerOrTeamName".eql?k or "playerOrTeamId".eql?k}
+            end
+            return tt
+        end
         
     end
 end
