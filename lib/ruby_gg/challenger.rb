6fa5ef4f72c0f0fa348872ea8bdc2e9c1ad908a2
Manish Shahi <manish.shahi@mnsu.edu>
2018-03-22 22:41:32 +0000

    changed the method name and now returns dto according to league api and you can limit how many results to get

--- lib/ruby_gg/challenger.rb
+++ lib/ruby_gg/challenger.rb
@@ -5,24 +5,31 @@ module RubyGg
     class Challenger
         attr_reader :challenger_url
         
-        def initialize(api_key, region)
+        def initialize(api_key, region) #Initializing the Challenger Instance. 
             @api_key = api_key
             @region = region
-            @base_url = "https://#{@region}1.api.riotgames.com"
+            @base_url = "https://#{@region}.api.riotgames.com"
             @challenger_url = "/lol/league/v3/challengerleagues/by-queue/"
         end
         
-        def solo_challengers #returns ID of top 5 solo/duo challenger players of the region.
-            queue = 'RANKED_SOLO_5x5'
-            final_url = "#{@base_url}#{@challenger_url}#{queue}?api_key=#{@api_key}"
-            payload = HTTParty.get(final_url)
-            challengers = payload['entries']
-            challengers.sort!{|x, y| y['leaguePoints'] <=> x['leaguePoints']}
-            id = []
-            (0...5).each{|n| id.push(challengers[n]['playerOrTeamId'])}
-            return id
+        def solo_queue (size = 0) # Not passing an argument / passing string (unless string is number)/ 0 => returns all 200 results.
+            if size.to_i == 0 || size.to_i >200
+                size = paycheck.size
+            else
+                size = size.to_i
+            end
+            paycheck = (HTTParty.get("#{@base_url}#{@challenger_url}RANKED_SOLO_5x5?api_key=#{@api_key}").parsed_response)['entries']
+            payload = []
+            paycheck.sort!{|x, y| y['leaguePoints'] <=> x['leaguePoints']}
+            (0...size).each {|x| payload.push(player_dto(paycheck[x]))}
+            return payload
         end
         
-        
+        private
+        def player_dto(player)
+            dto = {}
+            player.each{|k,v| dto[k.to_sym] = v }
+            return dto
+        end
     end
 end
