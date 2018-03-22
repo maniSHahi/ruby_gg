eb5ccf063681c77da9ce596b99cf067d1498e9ae
Manish Shahi <manish.shahi@mnsu.edu>
2018-03-22 22:40:19 +0000

    Added region_tag that handles regions for @base_url

--- lib/ruby_gg/client.rb
+++ lib/ruby_gg/client.rb
@@ -8,16 +8,28 @@ module RubyGg
         attr_reader :base_url
         def initialize(api_key, region = 'na')
             @api_key = api_key
-            @region = region
-            @base_url = "https://#{@region}1.api.riotgames.com"
+            region_tag = { :na => 'na1', 
+                            :eune => 'eun1', 
+                            :euw => 'euw1',
+                            :jp => 'jp1',
+                            :kr => 'kr',
+                            :lan => 'la1',
+                            :las => 'la2',
+                            :br => 'br1',
+                            :oce => 'oc1',
+                            :tr => 'tr1',
+                            :ru => 'ru',
+                            :pbe => 'pbe1' }
+            @region = region_tag[region.to_sym]
+            @base_url = "https://#{@region}.api.riotgames.com"
         end
     
         def challenger
-            @challenger = Challenger.new(api_key, region)
+            @challenger = Challenger.new(@api_key, @region)
         end
         
         def summoner
-            @summoner = Summoner.new(api_key, region)
+            @summoner = Summoner.new(@api_key, @region)
         end
     
     end
